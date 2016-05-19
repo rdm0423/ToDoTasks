b0e1657e86b37bbb1223cd146b3ca6e94187b3b8
Ross McIlwaine <rdm0423@gmail.com>
2016-05-19 17:25:36 -0600

    Adds UI

--- ToDoTasks/ViewController.swift
+++ ToDoTasks/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var dueDateTextField: UITextField!
+    @IBOutlet weak var taskNotesTextField: UITextView!
+    @IBOutlet var dueDatePicker: UIDatePicker!
+    @IBOutlet weak var taskSegmentedControl: UISegmentedControl!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func saveButtonTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func closeButtonTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func dueDatePickerValueChanged(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func taskPriorityValueChanged(sender: AnyObject) {
+        
     }
 
+    @IBAction func userTappedView(sender: AnyObject) {
+        
+    }
 
 }
 
